a969baece4e9d28e3d3f6a885af0ed77e46e8f15
Suma <48092912+Suma-xy@users.noreply.github.com>
2020-11-29 19:35:32 +0100

    Add files via upload

--- Birthday-Countdown/Birthday-Countdown.js
+++ Birthday-Countdown/Birthday-Countdown.js
@@ -1,5 +1,5 @@
 // Variables used by Scriptable.
 // These must be at the very top of the file. Do not edit.
-// icon-color: deep-blue; icon-glyph: magic;
+// icon-color: blue; icon-glyph: birthday-cake;
 // Version 0.0.1
 
