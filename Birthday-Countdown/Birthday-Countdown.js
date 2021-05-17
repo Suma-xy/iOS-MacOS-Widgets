2c1fa90e81cb0211600ab80e8f44dfc3320564f9
Suma <Suma_xy@icloud.com>
2021-05-17 11:46:54 +0200

    Update Birthday-Countdown.js

--- Birthday-Countdown/Birthday-Countdown.js
+++ Birthday-Countdown/Birthday-Countdown.js
@@ -1,7 +1,7 @@
 // Variables used by Scriptable.
 // These must be at the very top of the file. Do not edit.
 // icon-color: blue; icon-glyph: birthday-cake
-// Version 1.2.1
+// Version 1.0
 
 let dateForCountdown = ''
 let icon = ''
